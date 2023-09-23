062dd8a3a8371d9d614e012d559f3adcee48cfad
Islombek <eislombek9599@gmail.com>
2023-09-23 08:42:22 +0500

    Fix: Reja project

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const htpp = require("http");
 // 1. Kirish codelari
 
 app.use(express.static("public"));
-app.use(express.json);
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // 2. Sessions
@@ -16,16 +16,13 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4. Routing code
-
-app.get("/hello", function (req, res) {
-  //   res.end("Hello world!");
-  //   res.end(`<h1 style= 'background: blue' >Hello world!!!</h1>`);
-  res.end(`<h1 >Hello world!!!</h1>`);
+app.post("/create-item", (req, res) => {
+  console.log(req.body);
+  res.json({ test: "success" });
 });
-app.get("/git", function (req, res) {
-  //   res.end("Hello world!");
-  //   res.end(`<h1 style= 'background: blue' >Hello world!!!</h1>`);
-  res.end(`<h1 >Siz sovgalar sahiasidasiz!!!</h1>`);
+
+app.get("/", function (req, res) {
+  res.render("harid");
 });
 
 const server = htpp.createServer(app);
