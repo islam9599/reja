052132cd80d26e3b4cc182f8290a8fff37009383
Islombek <eislombek9599@gmail.com>
2023-09-25 15:45:12 +0500

    Fix : publicdagi muammo

--- server.js
+++ server.js
@@ -2,10 +2,21 @@ console.log("Web server boshlash!!!");
 const express = require("express");
 const app = express();
 const htpp = require("http");
+const fs = require("fs");
 
+let user;
+
+fs.readFile("database/user.json", "utf-8", (err, data) => {
+  if (err) {
+    console.log("ERROR", err);
+  } else {
+    user = JSON.parse(data);
+  }
+  console.log(data);
+});
 // 1. Kirish codelari
-
 app.use(express.static("public"));
+// console.log(express.static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,8 +27,12 @@ app.set("views", "views");
 app.set("view engine", "ejs");
 
 // 4. Routing code
+
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
 app.post("/create-item", (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   res.json({ test: "success" });
 });
 
