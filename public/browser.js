08728fe7e18c588cb61559356ced487d82db08b2
Islombek <eislombek9599@gmail.com>
2023-10-03 13:47:12 +0300

    Fix: Corrected browser.js

--- public/browser.js
+++ public/browser.js
@@ -1,15 +1,15 @@
 console.log("This is a front-end page");
-function itemTemplate(data) {
+function itemTemplate(item) {
   return `
        <li
           class="list-group-item list-group-item-info d-flex align-items-center justify-content-between"
         >
           <span class="item-text">${item.reja}</span>
           <div>
-            <button data_id = ${item._id} class="edit-me btn btn-secondary btn-sm mr-1">
+            <button data_id = "${item._id} "class="edit-me btn btn-secondary btn-sm mr-1">
               O'zgartirish
             </button>
-            <button data_id = ${item._id} class="delete-me btn btn-danger btn-sm">O'chirish</button>
+            <button data_id =" ${item._id} "class="delete-me btn btn-danger btn-sm">O'chirish</button>
           </div>
         </li>
     `;
@@ -21,18 +21,16 @@ let createForm = document.getElementById("create-form");
 console.log(createForm);
 createForm.addEventListener("submit", function (e) {
   e.preventDefault();
-  axios.post(
-    "/create-item",
-    { reja: createField.value }
-      .then((res) => {
-        document
-          .getElementById("item-list")
-          .insertAdjacentHTML("beforeend", itemTemplate(res.data));
-        createField.value("");
-        createField.focus();
-      })
-      .catch((err) => {
-        console.log("Please try again!!1");
-      })
-  );
+  axios
+    .post("/create-item", { reja: createField.value })
+    .then((res) => {
+      document
+        .getElementById("item-list")
+        .insertAdjacentHTML("beforeend", itemTemplate(res.data));
+      createField.value("");
+      createField.focus();
+    })
+    .catch((err) => {
+      console.log("Please try again!!1");
+    });
 });
